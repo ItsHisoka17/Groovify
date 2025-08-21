de95af030d4ad9764cd94c18deefe17a764cf79d
Jxdn <hashertechnical15@gmail.com>
2025-08-21 16:41:56 -0400

    Constants | Updated ENV

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -1,18 +1,23 @@
+require("dotenv").config();
 const ENV = process.env;
-const {CLIENT_ID, CLIENT_KEY} = ENV;
+const {CLIENT_ID, CLIENT_KEY, PORT} = ENV;
 
 const Constants = {
     BASE_AUTHORIZATION_URL: "https://accounts.spotify.com/authorize",
     BASE_API_URL: "https://api.spotify.com",
-    SCOPES: {
-        read: "user-read-private",
-        email: "user-email-private",
-        playback_position: "user-read-playback-position",
-        playback_top: "user-top-read",
-        playback_recent: "user-read-recently-played",
-        personalized: "user-personalized"
-    },
-    REDIRECT_URI: "https://moodify.com/authorize/callback"
+    SCOPES: [
+        "user-read-private",
+        "user-read-email",
+        /*
+        "user-read-playback-position",
+        "user-top-read",
+        "user-read-recently-played",
+        "user-personalized"*/
+    ],
+    REDIRECT_URI: "https://moodify.com/authorize/callback",
+    CLIENT_ID,
+    CLIENT_KEY,
+    PORT
 };
 
 
