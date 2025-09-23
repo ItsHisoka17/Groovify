54e61ea07e31841f7bd7fb173e1305ab64f31945
Jxdn <hashertechnical15@gmail.com>
2025-09-23 12:28:24 -0400

    Client | File update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
